c89907f599e84f2c7dabd4e2144ce89903498229
Rikelmy Aldubi <vn21007@ues.edu.sv>
2024-08-14 13:20:55 -0600

    adding search engine, paging, and sorting of data in provider and inventory.

--- src/views/proveedores/proveedores/BaseDataTable/index.js
+++ src/views/proveedores/proveedores/BaseDataTable/index.js
@@ -19,21 +19,28 @@ const BaseDataTable = forwardRef((props, ref) => {
 
 	const data = useSelector((state) => state.dataProveedores.data.data);
 	const loading = useSelector((state) => state.dataProveedores.data.loading);
-	const { current_page, perPage, total, search, sort } = useSelector((state) => state.dataProveedores.data.tableData);
+	const { page, paginate, total, search, sort } = useSelector((state) => state.dataProveedores.data.tableData);
 
 	const fetchData = useCallback(() => {
-		dispatch(getData({ reqParams: { current_page, perPage, sort, search }, reqUrl }));
+		dispatch(getData({ reqParams: { page, paginate, sort, search }, reqUrl }));
 		dispatch(setUrl(reqUrl));
-	}, [current_page, perPage, sort, search, reqUrl, dispatch]);
+	}, [page, paginate, sort, search, reqUrl, dispatch]);
 
-	useEffect(() => { fetchData() }, [fetchData, current_page, perPage, sort, search]);
+	useEffect(() => { fetchData() }, [fetchData, page, paginate, sort, search]);
 
-	const tableData = useMemo(() => ({ current_page, perPage, sort, search, total }), [current_page, perPage, sort, search, total]);
+	const tableData = useMemo(() => ({ page, paginate, sort, search, total }), [page, paginate, sort, search, total]);
 
-	const onPaginationChange = current_page => {
+	const onPaginationChange = page => {
 		const newTableData = cloneDeep(tableData);
-		newTableData.current_page = current_page;
+		newTableData.page = page;
 		dispatch(setTableData(newTableData));
+	}
+
+	const onSelectChange = value => {
+		const newTableData = cloneDeep(tableData)
+		newTableData.paginate =  Number(value)
+		newTableData.page = 1
+		dispatch(setTableData(newTableData))
 	}
 
 	const onSort = (sort, sortingColumn) => {
@@ -46,7 +53,7 @@ const BaseDataTable = forwardRef((props, ref) => {
 	const handleInputChanges = (val) => {
 		const newTableData = cloneDeep(tableData)
 		newTableData.search = val
-		newTableData.current_page = 1
+		newTableData.page = 1
 
 		if (typeof val === 'string' && val.length > 1) {
 			dispatch(setTableData(newTableData));
@@ -84,10 +91,12 @@ const BaseDataTable = forwardRef((props, ref) => {
 				skeletonAvatarColumns={[0]}
 				skeletonAvatarProps={{ width: 28, height: 28 }}
 				loading={loading}
-				pagingData={{ current_page, perPage, total }}
+				pagingData={{ page, paginate, total, search, sort }}
 				pageSizes={[5, 10, 25, 50, 100]}
 				onPaginationChange={onPaginationChange}
+				onSelectChange={onSelectChange}
 				onSort={onSort}
+				availableAll={true}
 			/>
 		</>
 	)
